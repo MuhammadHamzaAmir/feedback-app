3f8471e5e665bad81e65214218f44e6a9d3a2eb7
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-16 19:22:24 +0500

    FeedbacK Item component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
     <Header />
       <div className="container">
-        <h1>From App component</h1>
+        <FeedbackItem />
       </div>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types'
 
 function Header(props) {
-  return (
-        <div className="container">
-            <h2>{props.text}</h2>
+
+    const headerStyles = {
+        backgroundColor: props.bgColor,
+        color: props.textColor,
+    }
+    return (
+        <div style={headerStyles}>
+            <div className="container">
+                <h2>{props.text}</h2>
+            </div>
         </div>
-  )
+    )
 }
 
 
 Header.defaultProps = {
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95'
 }
 
 Header.propsTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string
 }
 export default Header
