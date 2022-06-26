abbdc564dc787495fdeafbbfb092135249ffe975
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-26 20:09:32 +0500

    Button component is created

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Button(props) {
+  
+    return (
+        <button type={props.type} disabled={props.isDisabled} className={`btn btn-${props.version}`}>
+            {props.children}
+        </button>
+  );
+}
+
+
+Button.defaultProps = {
+    version: "primary",
+    type: "button",
+    isDisabled: false,
+}
+
+Button.propTypes = {
+    childern : PropTypes.node.isRequired,
+    version : PropTypes.string,
+    type : PropTypes.string,
+    isDisabled : PropTypes.bool,
+}
+export default Button;
