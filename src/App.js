6e84cdba6968413128ffe3c6cc3a918f9d42857d
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-26 22:30:45 +0500

    Handling of feedback addition is added. New reviews are handled at app level state

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import FeedbackData from "./data/FeedbackData";
 import { useState } from "react";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import {v4 as uuidv4} from 'uuid';
 
 
 function App() {
 
     const [feedback, setFeedback] = useState(FeedbackData);
+
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback,...feedback]);
+    }
 
     const deleteFeedback = (id) =>{
         if (window.confirm("Are you sure you want to delete this feedback?")) {
@@ -21,7 +28,7 @@ function App() {
       <>
         <Header />
         <div className="container">
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback} />
             <FeedbackStats feedback={feedback}></FeedbackStats>
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
