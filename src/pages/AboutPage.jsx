44431b08b818bb22b2a2ae54c6a7a23e9fe6d8e5
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-27 22:02:32 +0500

    Link routing added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from "../components/shared/Card"
+import { Link } from "react-router-dom";
 
 function AboutPage() {
 
@@ -10,7 +11,7 @@ function AboutPage() {
 
         <p>Version 1.0</p>
 
-        <a href="/">Back to home</a>
+        <Link to="/">Back to home</Link>
       </div>
     </Card>
   );
