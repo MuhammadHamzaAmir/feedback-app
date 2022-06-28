be6a2919c14919197e193b26697625864668b934
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-28 20:32:05 +0500

    Edit Icon added

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,19 +1,22 @@
 import Card from "./shared/Card";
 // import PropTypes from "prop-types";
-import {FaTimes} from 'react-icons/fa'
+import {FaTimes,FaEdit} from 'react-icons/fa'
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
 
 function FeedbackItem(props) {
 
-    const {deleteFeedback} = useContext(FeedbackContext);
+    const {deleteFeedback,editFeedback} = useContext(FeedbackContext);
   
     return (
     <Card reverse={true}>
         <div className="num-display">{props.feedback.rating}</div>
         <button className="close" onClick={() => {deleteFeedback(props.feedback.id)}}>
           <FaTimes color="red"></FaTimes>
+        </button>
+        <button className="edit" onClick={() => {editFeedback(props.feedback)}}>
+            <FaEdit color="#FF5100"></FaEdit>
         </button>
         <div className="text-display">{props.feedback.text}</div>
     </Card>
