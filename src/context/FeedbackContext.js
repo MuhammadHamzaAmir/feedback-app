cebc3b76c1102432cf475d04507daddd484c744d
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-29 22:06:47 +0500

    Add feedback is integrated with the backend mock server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState,useEffect } from "react";
-import {v4 as uuidv4} from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -18,7 +17,7 @@ export const FeedbackProvider = (props) =>{
     },[])
 
     const fetchFeedback = async () => {
-        const response = await fetch(`http://localhost:4001/feedback?_sort=id&_order=desc`);
+        const response = await fetch(`/feedback?_sort=id&_order=desc`);
         const data = await response.json();
         setFeedback(data);
         setIsLoading(false);
@@ -30,9 +29,17 @@ export const FeedbackProvider = (props) =>{
 
         }
     };
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback,...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+
+        const data = await response.json();
+        setFeedback([data,...feedback]);
     }
 
     const updateFeedback = (id,updItem) => {
