0ca01632654d8d7b8714d8ffec7ae00a566ef95f
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-06-29 21:41:52 +0500

    Loading component integrated

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,24 +1,23 @@
 import FeedbackItem from "./FeedbackItem";
 import {useContext} from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
 
-    const {feedback} = useContext(FeedbackContext);
+    const {feedback,isLoading} = useContext(FeedbackContext);
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <div>No feedback yet</div>
     }
 
 
-    return (
-      <>
+    return (isLoading ? <Spinner/> : (<>
       <div className="feedback-list">
         {feedback.map(item => (
             <FeedbackItem key={item.id} feedback={item}/>
         ))}
         </div>
-      </>
-    );
+      </>))
 }
 export default FeedbackList;
